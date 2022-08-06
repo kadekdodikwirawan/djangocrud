31f9150ac9d389b3fc4439d3982f123759a604dc
Kadek Dodik Wirawan <dodikdogen@gmail.com>
2022-08-06 20:48:26 +0800

    fibonacci view

--- fibonacci/views.py
+++ fibonacci/views.py
@@ -1,10 +1,15 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 import numpy as np
 
 def index(request):
     if(request.GET.get('number') ):
         fibo_numbers =  fibonacci(int(request.GET.get('number')))
-        return HttpResponse(fibo_numbers)
+        return render(request, "fibonacci.html", {
+            'fibonacci': fibo_numbers,
+            'n': len(fibo_numbers)
+        })
+        # return HttpResponse(fibo_numbers)
     else:
         return HttpResponse('Masukan parameter number pada url (contoh: http://127.0.0.1:8000/fibonacci/?number=8)')
 def fibonacci(value):
@@ -13,9 +18,9 @@ def fibonacci(value):
     n1 = 0
     n2 = 1
     while count < value:
-        fibo_number.append(str(n1)+",")
+        fibo_number.append(n1)
         last_number = n1 + n2
         n1 = n2
         n2 = last_number
         count += 1
-    return np.array(fibo_number)
+    return fibo_number
